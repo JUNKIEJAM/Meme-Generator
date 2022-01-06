eb09b83aa391e1fae7f94fa78d621ca0e66ce048
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-06 23:30:42 +0530

    JS rectification

--- main.js
+++ main.js
@@ -1,67 +1,65 @@
-const imageFileInput=document.querySelector("#imageFileInput");
-const topTextInput=document.querySelector("#topTextInput")
-const bottomTextInput=document.querySelector("#bottomTextInput")
-const canvas=document.querySelector("#meme");
+const imageFileInput = document.querySelector("#imageFileInput");
+const canvas = document.querySelector("#meme");
+const topTextInput = document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomTextInput");
 
 let image;
 
-imageFileInput.addEventListener("change",()=>{
+imageFileInput.addEventListener("change", (e) => {
+  const imageDataUrl = URL.createObjectURL(e.target.files[0]);
 
-    const imageDataUrl=URL.createObjectURL(imageFileInput.files[0]);
-   
-    //standard HTML image element
-    image=new Image();
-    image.src=imageDataUrl;
+  image = new Image();
+  image.src = imageDataUrl;
 
-    image.addEventListener("load",()=>{
-        updateMemeCanvas(canvas,image,topTextInput.value,bottomTextInput.value);
-
-    },{once:true});
-
-
+  image.addEventListener(
+    "load",
+    () => {
+      updateMemeCanvas(
+        canvas,
+        image,
+        topTextInput.value,
+        bottomTextInput.value
+      );
+    },
+    { once: true }
+  );
 });
 
-topTextInput.addEventListener("change",()=>{
-    updateMemeCanvas(canvas,image,topTextInput.value,bottomTextInput.value);
-
+topTextInput.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
 });
 
-bottomTextInput.addEventListener("change",()=>{
-    updateMemeCanvas(canvas,image,topTextInput.value,bottomTextInput.value);
+bottomTextInput.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
 
-})
+function updateMemeCanvas(canvas, image, topText, bottomText) {
+  const ctx = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
 
-function updateMemeCanvas(canvas,image,topText,bottomText){
-   
-    const ctx=canvas.getContext("2d");
-    const width=image.width;
-    const height=image.height;
-    const fontSize=Math.floor(width/10);
-    const yOffset=height/25;
+  // Update canvas background
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0);
 
-    canvas.width=width;
-    canvas.height=height;
-    ctx.drawImage(image,0,0);
-    
-    ctx.strokeStyle="black";
-    ctx.lineWidth=Math.floor(fontSize/4);
-    ctx.fillStyle="white";
-    ctx.textAlign="center";
-    cx.lineJoin="round";
-    ctx.font=`${fontSize}px sans-serif`;
+  // Prepare text
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = Math.floor(fontSize / 4);
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.lineJoin = "round";
+  ctx.font = `${fontSize}px sans-serif`;
 
-    //add top text
-    ctx.textBaseline="top";
-   
-    ctx.StrokeText(topText,width/2,yOffset);
-    ctx.fillText(topText,width/2,yOffset);
+  // Add top text
+  ctx.textBaseline = "top";
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
 
-    //add bottom text
-    ctx.textBaseline="bottom";
-   
-    ctx.StrokeText(bottomText,width/2,height-yOffset);
-    ctx.fillText(bottomText,width/2,height-yOffset);
-    
+  // Add bottom text
+  ctx.textBaseline = "bottom";
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
 }
-
-
